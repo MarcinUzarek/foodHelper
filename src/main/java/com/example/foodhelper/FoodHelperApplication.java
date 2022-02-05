e62ae8f3c2335fe6e99f326affbbdd52d89931ee
MarcinUzarek <uzarek.marcin01@gmail.com>
2022-02-05 02:49:13 +0100

    Initial commit

---
+++ src/main/java/com/example/foodhelper/FoodHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/foodhelper/FoodHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

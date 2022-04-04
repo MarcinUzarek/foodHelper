35ad2063b7eb72921fa7a80ac706f2395b212c4f
MarcinUzarek <uzarek.marcin01@gmail.com>
2022-04-04 22:36:43 +0200

    add UserDetailsServiceImpl tests

--- src/main/java/com/example/foodhelper/user_details/UserDetailsServiceImpl.java
+++ src/main/java/com/example/foodhelper/user_details/UserDetailsServiceImpl.java
@@ -2,7 +2,6 @@ package com.example.foodhelper.user_details;
 
 import com.example.foodhelper.model.User;
 import com.example.foodhelper.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,8 +10,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
---
+++ src/test/java/com/example/foodhelper/user_details/UserDetailsServiceImplTest.java
@@ -0,0 +1,45 @@
+package com.example.foodhelper.user_details;
+
+import com.example.foodhelper.TestDataSample;
+import com.example.foodhelper.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class UserDetailsServiceImplTest implements TestDataSample {
+
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsService;
+    @Mock
+    private UserRepository userRepository;
+
+
+
+    @Test
+    void should_load_user_details_by_username() {
+        //given
+        String email = "first@email.com";
+        var user = getSampleDataForUsers().get(0);
+        given(userRepository.findByEmail(email))
+                .willReturn(java.util.Optional.ofNullable(user));
+
+        //when
+        var userDetails = userDetailsService.loadUserByUsername(email);
+
+        //then
+        assertThat(userDetails, instanceOf(UserDetails.class));
+        assertThat(user.getName(), is("First"));
+
+    }
+}

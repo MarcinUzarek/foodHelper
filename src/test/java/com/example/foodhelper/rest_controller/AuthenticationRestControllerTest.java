f886e230770cb00783fa85b668565d32a2252e1f
MarcinUzarek <uzarek.marcin01@gmail.com>
2022-05-27 22:53:32 +0200

    Add tests to AuthenticationRestController.register()

--- src/test/java/com/example/foodhelper/rest_controller/AuthenticationRestControllerTest.java
+++ src/test/java/com/example/foodhelper/rest_controller/AuthenticationRestControllerTest.java
@@ -1,52 +1,88 @@
 package com.example.foodhelper.rest_controller;
 
+import com.example.foodhelper.exception.custom.EmailAlreadyExistsException;
 import com.example.foodhelper.model.dto.UserRegisterDTO;
 import com.example.foodhelper.service.UserService;
+import com.example.foodhelper.user_details.UserDetailsServiceImpl;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.http.HttpStatus.ALREADY_REPORTED;
+import static org.springframework.http.HttpStatus.OK;
 
 
 @WebMvcTest(AuthenticationRestController.class)
 class AuthenticationRestControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private WebApplicationContext webApplicationContext;
 
     @MockBean
     private UserService userService;
 
+    @MockBean
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+
+
     @Test
-    void context_should_load() {
-        assertTrue(true);
+    void should_create_user_when_not_registered_yet() {
+        //given
+        UserRegisterDTO user = createUserRegisterDto();
+
+        BDDMockito.given(userService.createUser(user))
+                .willReturn(user);
+
+        //when then
+        given()
+                .webAppContextSetup(webApplicationContext)
+                .auth().none()
+                .body(user).contentType(ContentType.JSON)
+        .when()
+                .post("/api/register")
+                .then()
+                .statusCode(OK.value())
+                .and()
+                .body("email", equalTo("test@gmail.com"));
     }
 
-//    @Test
-//    void should_create_user() throws Exception {
-//        //given
-//        var userRegisterDTO = new UserRegisterDTO();
-//        userRegisterDTO.setName("name");
-//        userRegisterDTO.setEmail("test@gmail.com");
-//        userRegisterDTO.setPassword("pass");
-//
-//        BDDMockito.given(userService.createUser(userRegisterDTO))
-//                .willReturn(userRegisterDTO);
-//
-//        //when then
-//        mockMvc.perform(post("/api/register")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content("{\"name\" : \"name\", \"email\" : \"test@gmail.com\",\"password\" : \"pass\"}"))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//    }
+    @Test
+    void should_throw_if_user_with_given_email_already_exists() {
+        //given
+        UserRegisterDTO user = createUserRegisterDto();
+
+        BDDMockito.given(userService.createUser(user))
+                .willThrow(new EmailAlreadyExistsException());
+
+        //when then
+        given()
+                .webAppContextSetup(webApplicationContext)
+                .auth().none()
+                .body(user).contentType(ContentType.JSON)
+        .when()
+                .post("/api/register")
+                .then()
+                .statusCode(ALREADY_REPORTED.value())
+                .and()
+                .body("status", is("ALREADY_REPORTED"));
+    }
+
+    private UserRegisterDTO createUserRegisterDto() {
+        var user = new UserRegisterDTO();
+        user.setName("name");
+        user.setEmail("test@gmail.com");
+        user.setPassword("pass");
+        return user;
+    }
 
 }
 
